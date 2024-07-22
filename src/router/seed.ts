a63b6b05b45775ee6b8f6e9b67d51b49014de4b5
toastycoder0 <silentworker36743@joseluria.com>
2024-07-22 15:54:35 -0500

    feat: added seed api endpoint

---
+++ src/router/seed.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { ENV } from '../constants/env'
+import db from '../db'
+import data from '../constants/data.json'
+
+const router = Router()
+
+const { NODE_ENV } = process.env
+
+router.post('/', async (_, res) => {
+  if (NODE_ENV === ENV.production) {
+    return res.status(500).json({ message: 'This action cannot be performed in a production environment.' })
+  }
+
+  await db.product.createMany({ data })
+
+  res.status(200).json({ message: 'Data created' })
+})
+
+export { router }
