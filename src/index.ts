6984d23fbe918a01dd4a91d672ef9acf0b36f43a
toastycoder0 <silentworker36743@joseluria.com>
2024-07-22 19:48:30 -0500

    feat: implemented swagger ui on main endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import swaggerUI from 'swagger-ui-express'
 import router from './router'
+import docs from './constants/docs.json'
 
 const { PORT = 4000 } = process.env
 
@@ -11,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api', router)
+app.use('/', swaggerUI.serve, swaggerUI.setup(docs))
 
 app.listen(PORT, () => {
   console.log(`🟢 Server running on PORT: ${PORT}`)
