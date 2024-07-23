4fd259c41d9ab65c2f6a56e703ecdb36d887da3c
toastycoder0 <silentworker36743@joseluria.com>
2024-07-23 14:44:39 -0500

    refactor: implemented external express server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import swaggerUI from 'swagger-ui-express'
-import router from './router'
-import docs from './constants/docs.json'
+import { app } from './server'
 
 const { PORT = 4000 } = process.env
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api', router)
-app.use('/', swaggerUI.serve, swaggerUI.setup(docs))
 
 app.listen(PORT, () => {
   console.log(`🟢 Server running on PORT: ${PORT}`)
