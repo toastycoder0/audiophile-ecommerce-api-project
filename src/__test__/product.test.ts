80815cf135d94fd2f5ec230cd610da0476456f78
toastycoder0 <silentworker36743@joseluria.com>
2024-07-23 15:56:20 -0500

    feat: mocked db calls on testing

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -1,24 +1,45 @@
 import request from 'supertest'
 import { app } from '../server'
+import prisma from '../db'
+import { product } from '../constants/mock.json'
+
+jest.mock('../db', () => ({
+  product: {
+    findMany: jest.fn(),
+    findFirst: jest.fn()
+  }
+}))
+
+const db = jest.mocked(prisma)
 
 describe('Product API', () => {
+  beforeEach(() => jest.clearAllMocks())
+
   it('should fetch all products', async () => {
+    db.product.findMany.mockResolvedValue([product])
+
     const res = await request(app).get('/api/product')
-
-    expect(res.status).toBe(200)
-    expect(res.body).toHaveProperty('products')
-  })
-
-  it('should fetch products by category', async () => {
-    const category = 'earphones'
-    const res = await request(app).get(`/api/product?category=${category}`)
 
     expect(res.status).toBe(200)
     expect(res.body).toHaveProperty('products')
     expect(res.body.products).toBeInstanceOf(Array)
   })
 
+  it('should fetch products by category', async () => {
+    db.product.findMany.mockResolvedValue([product])
+
+    const category = 'earphones'
+    const res = await request(app).get(`/api/product?category=${category}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveProperty('products')
+    expect(res.body.products).toBeInstanceOf(Array)
+    expect(res.body.products).not.toHaveLength(0)
+  })
+
   it('should fetch a product by slug', async () => {
+    db.product.findFirst.mockResolvedValue(product)
+
     const slug = 'yx1-earphones'
     const res = await request(app).get(`/api/product/${slug}`)
 
@@ -27,6 +48,8 @@ describe('Product API', () => {
   })
 
   it('should return 404 for a non-existing product', async () => {
+    db.product.findFirst.mockResolvedValue(null)
+
     const slug = 'non-existing-product'
     const res = await request(app).get(`/api/product/${slug}`)
 
